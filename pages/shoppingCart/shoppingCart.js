de99d9786b879c03e422a971bf246da437268756
S1mpIe <S1mpIe@users.noreply.github.com>
2020-05-05 18:41:03 +0800

    update

--- pages/shoppingCart/shoppingCart.js
+++ pages/shoppingCart/shoppingCart.js
@@ -1,13 +1,17 @@
 // pages/shoppingCart/shoppingCart.js
 Page({
   data:{
+    accessToken:'',
     goodsList:[],
     totalPrice:0,
   },
-  onLoad(query) {
+  onShow(query) {
     wx.getStorage({
       key:"accessToken",
       success:(result => {
+        this.setData({
+          accessToken:result.data
+        });
         wx.request({
           url:'http://www.s1mpie.top:8080/sxdj/shoppingCart',
           method:'get',
@@ -27,8 +31,35 @@ Page({
       })
     })
   },
+  deleteThisGoods(e){
+    var cart=this.data.goodsList;
+    var item=cart[e.currentTarget.dataset.index];
+    console.log(item);
+    wx.request({
+      url:"http://www.s1mpIe.top:8080/sxdj/shoppingCart?id="+item.id,
+      method:'delete',
+      header:{
+        accessToken:this.data.accessToken
+      },success:(result => {
+        cart.splice(e.currentTarget.dataset.index,1);
+        this.flushCart(cart);
+      })
+    })
+  },
   handleChange(e){
-    console.log(e)
+    var cart = this.data.goodsList;
+    var index = e.currentTarget.dataset.index;
+    cart[index].selectNumber=e.detail.value;
+    wx.request({
+      url:"http://www.s1mpie.top:8080/sxdj/shoppingCart?id="+cart[index].id+"&number="+e.detail.value,
+      method:'post',
+      header:{
+        accessToken:this.data.accessToken
+      },
+      success:(result => {
+        this.flushCart(cart);
+      })
+    })
   },
   flushCart(Cart){
     this.setData({
